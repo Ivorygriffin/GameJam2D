da477b6cd4ada2adc5bec622f4d4ebf903a82361
Ivorygriffin <81143450+Ivorygriffin@users.noreply.github.com>
2022-04-19 19:01:08 +0930

    build

--- New Unity Project/Assets/Assets/Scripts/GameController.cs
+++ New Unity Project/Assets/Assets/Scripts/GameController.cs
@@ -32,6 +32,7 @@ public class GameController : MonoBehaviour
     public GameObject slider2;
     public GameObject slider3;
     public GameObject refreshScreen, antiRefresh, screen1, screen2,screen3, gameoverScreen, winScreen, pauseMenu;
+    public GameObject Canonn;
 
     [Header("Sliders")]
     public Slider s1, s2, s3;
@@ -44,6 +45,7 @@ public class GameController : MonoBehaviour
 
     [Header("bools")]
     private bool _slider1, _slider2, _slider3;
+    private bool hasrun1, hasrun2;
 
     
     
@@ -60,10 +62,11 @@ public class GameController : MonoBehaviour
     void Update()
     {
         ScoreTextUpdate();
-        //MountainClimbing();
+        MountainClimbing();
         PointCheck();
         BallText();
         PauseMenu();
+        LevelComplete();
         
         
     }
@@ -76,38 +79,37 @@ public class GameController : MonoBehaviour
 
   
 
-    //public void MountainClimbing()
-    //{
-    //    // UI person climbs more depending on score collected
-    //    //slider moves stick figure up 
-    //    if (_slider1 == true && currentScore != s1Max)
-    //    {
-    //        s1.value = currentScore;
-    //    }
-    //    if (currentScore == s1Max && _slider1 == true)
-    //    {
-    //        slider1.SetActive(false);
-    //        _slider1 = false;
-    //        slider2.SetActive(true);
-    //        _slider2 = true;
-    //    }
-    //    if(_slider2 == true && currentScore != s2Max)
-    //    {
-    //        s2.value = currentScore;
-    //    }
-    //    if (currentScore == s2Max && _slider2 == true)
-    //    {
-    //        slider2.SetActive(false);
-    //        _slider2 = false;
-    //        slider3.SetActive(true);
-    //        _slider3 = true;
-    //    }
-    //    if(_slider3 == true && currentScore != s3Max)
-    //    {
-    //        s3.value = currentScore;
-    //    }
+    public void MountainClimbing()
+    {
+         
+        if (_slider1 == true && currentScore < s1Max)
+        {
+            s1.value = currentScore;
+        }
+        if (currentScore >= s1Max && _slider1 == true)
+        {
+            slider1.SetActive(false);
+            _slider1 = false;
+            slider2.SetActive(true);
+            _slider2 = true;
+        }
+        if (_slider2 == true && currentScore < s2Max)
+        {
+            s2.value = currentScore;
+        }
+        if (currentScore >= s2Max && _slider2 == true)
+        {
+            slider2.SetActive(false);
+            _slider2 = false;
+            slider3.SetActive(true);
+            _slider3 = true;
+        }
+        if (_slider3 == true && currentScore < s3Max)
+        {
+            s3.value = currentScore;
+        }
 
-    //}
+    }
     public void BallText()
     {
         BallsRemaining.text = currentBall.ToString();
@@ -145,9 +147,10 @@ public class GameController : MonoBehaviour
     public void LevelComplete()
     {
         //third camp reached triggers end of level 
-        if(currentScore == finalCampScore)
+        if(currentScore >= finalCampScore)
         {
             winScreen.SetActive(true);
+            Canonn.SetActive(false);
             Debug.Log("levelComplete");
         }
     }
@@ -155,21 +158,26 @@ public class GameController : MonoBehaviour
     public void GameOver()
     {
         gameoverScreen.SetActive(true);
+        Canonn.SetActive(false);
+        
     }
 
 
     public void PointCheck()
     {
         //checks if number of points reaches a camp
-        if(currentScore == camp1Score)
+        if(currentScore >= camp1Score && hasrun1 == false)
         {
             Socialising();
             currentBall = 0;
+            hasrun1 = true;
             
         }
-        if(currentScore == camp2Score)
+        if(currentScore >= camp2Score && hasrun2 == false)
         {
             Relaxing();
+            currentBall = 0;
+            hasrun2 = true;
         }
 
 
